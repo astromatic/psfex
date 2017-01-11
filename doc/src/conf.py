0f2df26c70056b47fcf2bde9e286ffe30682dd45
Emmanuel Bertin <bertin@localhost.localdomain>
2017-01-11 15:02:02 +0100

    Switched off debuginfo build in spec file.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -76,7 +76,7 @@ language = None
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-today = 'Tue Oct 25 2016'
+today = 'Wed Jan 11 2017'
 # Else, today_fmt is used as the format for a strftime call.
 #today_fmt = '%B %d, %Y'
 
